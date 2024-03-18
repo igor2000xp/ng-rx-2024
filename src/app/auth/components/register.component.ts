04d69153c419b827529bbf4ecaac97b41c9594a3
igor2000xp <igorring@gmail.com>
2024-03-18 23:17:33 +0100

    feat: add form layout

--- src/app/auth/components/register.component.ts
+++ src/app/auth/components/register.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'mc-register',
+//   templateUrl: './register.component.html',
+//   standalone: true,
+// })
+// export class RegisterComponent {
+
+// }
+
+import {CommonModule} from '@angular/common'
+import {Component} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {RouterLink} from '@angular/router'
+// import {Store} from '@ngrx/store'
+import {combineLatest} from 'rxjs'
+// import {BacknedErrorMessages} from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component'
+// import {authActions} from '../../store/actions'
+// import {selectIsSubmitting, selectValidationErrors} from '../../store/reducers'
+// import {RegisterRequestInterface} from '../../types/registerRequest.interface'
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
   standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    // BacknedErrorMessages,
+  ],
 })
 export class RegisterComponent {
-  
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  // data$ = combineLatest({
+  //   isSubmitting: this.store.select(selectIsSubmitting),
+  //   backendErrors: this.store.select(selectValidationErrors),
+  // })
+
+  constructor(
+    private fb: FormBuilder, 
+    // private store: Store,
+    ) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue())
+    // const request: RegisterRequestInterface = {
+    //   user: this.form.getRawValue(),
+    // }
+    // this.store.dispatch(authActions.register({request}))
+  }
 }
+
