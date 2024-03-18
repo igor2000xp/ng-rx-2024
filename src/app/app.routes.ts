6465730efe287eadd4c69923fdf4024d5d908b28
igor2000xp <igorring@gmail.com>
2024-03-18 23:00:07 +0100

    feat: create draft of the register section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
-export const routes: Routes = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'register',
+    loadChildren: ()=> import('./auth/auth.routes').then((m) => m.registerRoutes),
+  },
+];
---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Route } from '@angular/router';
+import { RegisterComponent } from './components/register.component';
+
+export const registerRoutes: Route[] = [
+  {
+    path:'', component: RegisterComponent,
+  },
+];
