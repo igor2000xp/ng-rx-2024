7c3018a74bdf5bd6eccabae29c600cc1b1ead626
igor2000xp <igorring@gmail.com>
2024-03-25 10:50:48 +0100

    feat: create Persistence function for localStorage and Effect for that

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -1,18 +1,21 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { inject } from '@angular/core';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { authActions } from './actions';
 import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { PersistanceService } from '../../shared/services/persistance.service';
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService$ = inject(AuthService)) => {
+  (actions$ = inject(Actions), authService$ = inject(AuthService), persistanceService = inject(PersistanceService)) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({ request }) => {
         return authService$.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('Access token', currentUser.token);
             return authActions.registerSuccess({ currentUser });
           }),
           catchError((errorResponse: HttpErrorResponse) => {
@@ -27,4 +30,19 @@ export const registerEffect = createEffect(
     );
   },
   { functional: true }
+);
+
+export const RedirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      })
+    );
+  },
+  {
+    functional: true,
+    dispatch: false,
+  }
 );
